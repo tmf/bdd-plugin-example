c58158b3756f38b419a4ef8bd3345c5bec34615f
tmf <tom.forrer@gmail.com>
2014-11-02 19:08:10 +0100

    rename features folder (case stuff on osx)

--- features-rename/Context/PluginFeatureContext.php
+++ features-rename/Context/PluginFeatureContext.php
@@ -4,12 +4,9 @@
  * @copyright Copyright (c) 2014 Tom Forrer (http://github.com/tmf)
  */
 
-namespace Tmf\PluginExample\Features\Context;
-
 use \Behat\Behat\Context\SnippetAcceptingContext;
 use \Behat\MinkExtension\Context\MinkContext;
 use Tmf\WordPressExtension\Context\WordPressContext;
-
 
 /**
  * Class PluginFeatureContext
